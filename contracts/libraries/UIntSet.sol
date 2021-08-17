85f64f366204b936fa090e20809ac357fb13d57d
0xmu <0xmu@protonmail.com>
2021-08-17 12:28:14 +0100

    Updates

---
+++ contracts/libraries/UIntSet.sol
@@ -0,0 +1,72 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+library UintSet {
+
+    struct Set {
+        uint256[] _values;
+
+        // Position of the value in the `values` array, plus 1 because index 0 means a value is not in the set
+        mapping (uint256 => uint256) _indexes;
+    }
+
+    function add(Set storage set, uint256 value) internal returns (bool) {
+        if (!contains(set, value)) {
+            set._values.push(value);
+            // The value is stored at length-1, but we add 1 to all indexes
+            // and use 0 as a sentinel value
+            set._indexes[value] = set._values.length;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function remove(Set storage set, uint256 value) internal returns (bool) {
+        // We read and store the value's index to prevent multiple reads from the same storage slot
+        uint256 valueIndex = set._indexes[value];
+
+        if (valueIndex != 0) { // Equivalent to contains(set, value)
+            // To delete an element from the _values array in O(1), we swap the element to delete with the last one in
+            // the array, and then remove the last element (sometimes called as 'swap and pop').
+            // This modifies the order of the array, as noted in {at}.
+
+            uint256 toDeleteIndex = valueIndex - 1;
+            uint256 lastIndex = set._values.length - 1;
+
+            // When the value to delete is the last one, the swap operation is unnecessary. However, since this occurs
+            // so rarely, we still do the swap anyway to avoid the gas cost of adding an 'if' statement.
+
+            uint256 lastvalue = set._values[lastIndex];
+
+            // Move the last value to the index where the value to delete is
+            set._values[toDeleteIndex] = lastvalue;
+            // Update the index for the moved value
+            set._indexes[lastvalue] = toDeleteIndex + 1; // All indexes are 1-based
+
+            // Delete the slot where the moved value was stored
+            set._values.pop();
+
+            // Delete the index for the deleted slot
+            delete set._indexes[value];
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function contains(Set storage set, uint256 value) internal view returns (bool) {
+        return set._indexes[value] != 0;
+    }
+
+    function length(Set storage set) internal view returns (uint256) {
+        return set._values.length;
+    }
+
+    function at(Set storage set, uint256 index) internal view returns (uint256) {
+        require(set._values.length > index, 'UintEnumerableSet: OUT_OF_BOUNDS');
+        return set._values[index];
+    }
+
+}
